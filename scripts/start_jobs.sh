9a8e9375fedb38775eac55ee2109e3aa4b301f18
Sp00nyMan <mrstormproxdd@gmail.com>
2023-07-05 15:23:19 +0200

    minor fix

--- scripts/start_jobs.sh
+++ scripts/start_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sh scripts/clear.sh
-sbatch --output=log_plain.txt job_script.sh plain
-sbatch --output=log_flow.txt job_script.sh flow
+sbatch --output=log_plain.txt scripts/job_script.sh plain
+sbatch --output=log_flow.txt scripts/job_script.sh flow
 # sbatch --output=log_convex.txt job_script.sh convex
